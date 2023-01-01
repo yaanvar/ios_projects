bfb4f6fc7d081b9ed0ecc7e3af6c76cf3c9aa87b
yaanvar <54923760+yaanvar@users.noreply.github.com>
2023-01-01 12:34:03 +0300

    GitHubFollower: project init

---
+++ GitHubFollowers/GitHubFollowers/FavoritesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesViewController.swift
+//  GitHubFollowers
+//
+//  Created by Anvar Rahimov on 01.01.2023.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+}
