8233d7ddd1058e2147d7a0104ecb6038c49e4df9
yaanvar <54923760+yaanvar@users.noreply.github.com>
2023-01-03 13:05:29 +0300

    GitHub Followers: codable models

---
+++ GitHubFollowers/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Anvar Rahimov on 03.01.2023.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    let login: String
+    let avatarURL: String
+}
---
+++ GitHubFollowers/GitHubFollowers/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Anvar Rahimov on 03.01.2023.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
